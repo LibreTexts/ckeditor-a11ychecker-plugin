8438e84987af1aa0c636f44948d4c7e7f0f3fdad
Alvin Agana <alvinagana@Alvins-MacBook-Pro.local>
2022-03-28 13:55:46 -0700

    figured out how to link custom issues

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -19,6 +19,32 @@ const registerPlugin = () => {
   //CKEDITOR.addCss( '.cke_a11yc_ui_button_ignore_wrapper { border: 10px solid red !important }' );
   //CKEDITOR.addCss( 'div.cke_a11yc_ui_button_wrapper.cke_a11yc_ui_button_ignore_wrapper { display: none; !important }' );
 
+  CKEDITOR.on("instanceReady", function() {
+    var a11ychecker = CKEDITOR.plugins.a11ychecker;
+    a11ychecker.IssueDetails.avoidStrongs = {
+      title: 'Avoid strongs',
+      descr: 'Our users do not like <strong>strongs</strong>, use <em>emphasize</em> instead 😉'
+    };
+
+    a11ychecker.Engine.prototype.on( 'process', function( evt ) {
+      // This is where the actual checking occurs, and this is where you want to report custom issues.
+      var Issue = a11ychecker.Issue,
+          contentElement = evt.data.contentElement,
+          issues = evt.data.issues
+      CKEDITOR.tools.array.forEach( contentElement.find( 'strong' ).toArray(), function( strong ) {
+          issues.addItem( new Issue( {
+              originalElement: strong,
+              testability: Issue.testability.NOTICE,
+              id: 'avoidStrongs',
+              details: {
+                title: 'Avoid strongs',
+                descr: 'Our users do not like <strong>strongs</strong>, use <em>emphasize</em> instead 😉'
+              }
+          }, a11ychecker.Engine ) );
+      } );   
+    } );
+  })
+
 
   // TODO: a11yButton is no longer needed..i think lol
   
