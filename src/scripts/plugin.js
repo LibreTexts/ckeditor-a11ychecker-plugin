ec19f064baa47acd32dd64556a0dd479ef211491
Alvin Agana <alvagana@ucdavis.edu>
2022-05-20 14:08:44 -0700

    changed outline of focused elements in ckeditor

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -24,7 +24,7 @@ function addCss() {
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_focused.cke_a11yc_error.cke_a11yc_issue { outline:5px dashed #ff6863 !important; }' );
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_focused.cke_a11yc_warning.cke_a11yc_issue { outline:5px dashed #f3994a !important; }' );
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_focused.cke_a11yc_notice.cke_a11yc_issue { outline:5px dashed #68bb59 !important; }' );
-  CKEDITOR.addCss( '.cke_editable .cke_a11yc_issue { outline:3px dashed #03a9f4 !important; }' );
+  CKEDITOR.addCss( '.cke_editable .cke_a11yc_issue { outline:3px dotted #03a9f4 !important; }' );
 }
 
 export default registerPlugin;
