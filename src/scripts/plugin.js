96f0fa95e6135c07d464922de62d2f961a0ac38f
Alvin Agana <alvagana@ucdavis.edu>
2022-05-24 15:58:31 -0700

    added comments

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -3,33 +3,43 @@ import "../styles/contents.css";
 import loadPlugin from "./loadPlugin";
 import loadCustomFixes from "./customFixes";
 import addA11yChecker from "./a11ychecker";
+
+// Main
 const registerPlugin = () => {
     addBalloonPanel();
     addA11yChecker();
     loadPlugin();
     loadCustomFixes();
+    changeMagicLineConfig();
     addCss();
-
-    // Changing magic line config
-    CKEDITOR.dtd.$block['figure'] = 1;
-    CKEDITOR.config.magicline_everywhere = true;
-
-  if (CKEDITOR.config.extraPlugins === '') {
-    CKEDITOR.config.extraPlugins += 'a11ychecker,a11yButton';
-  } else {
-    CKEDITOR.config.extraPlugins += ',a11ychecker,a11yButton';
-  }
-
+    register();
 };
 
-
 // Adding any CSS styles to the editor.
-// This is where we change the blue/red/orange borders of the balloon panel
+// This is where we change the blue/red/orange borders of the focused issues
 function addCss() {
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_focused.cke_a11yc_error.cke_a11yc_issue { outline:5px dashed #ff6863 !important; }' );
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_focused.cke_a11yc_warning.cke_a11yc_issue { outline:5px dashed #f3994a !important; }' );
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_focused.cke_a11yc_notice.cke_a11yc_issue { outline:5px dashed #68bb59 !important; }' );
   CKEDITOR.addCss( '.cke_editable .cke_a11yc_issue { outline:3px dotted #03a9f4 !important; }' );
+}
+
+// Changing the magic line config
+function changeMagicLineConfig() {
+    // Ensures that figures are considered block level (see https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_dtd.html)
+    CKEDITOR.dtd.$block['figure'] = 1;
+
+    // Adds magic line to all block level elements that are specifid in CKEDITOR.dtd.$block
+    CKEDITOR.config.magicline_everywhere = true;
+}
+
+// Registering the plugins
+function register() {
+  if (CKEDITOR.config.extraPlugins === '') {
+    CKEDITOR.config.extraPlugins += 'a11ychecker,a11yButton';
+  } else {
+    CKEDITOR.config.extraPlugins += ',a11ychecker,a11yButton';
+  }
 }
 
 export default registerPlugin;
--- src/scripts/registerA11yPluginLibretexts.js
+++ src/scripts/registerA11yPluginLibretexts.js
@@ -4,14 +4,7 @@
 import registerPlugin from './plugin';
 
 LibreEditor.a11yPlugin = (config) => {
-  registerPlugin();
-  config.toolbar[12].push('a11yButton');
+  registerPlugin();                       // Calling the main function to register a11ychecker
+  config.toolbar[12].push('a11yButton');  // Pushing the plugin's icon onto the CKEditor's toolbar
 };
-
-// Use if changing config settings
-/*
-LibreEditor.editorConfig = function ( config ) {
-
-}
-*/
 
